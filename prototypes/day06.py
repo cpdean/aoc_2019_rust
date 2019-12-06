dc5be459113304374829ebcc74d2b4e07f356838
Conrad Dean <conrad.p.dean@gmail.com>
2019-12-06 11:39:14 -0500

    finally got it

--- prototypes/day06.py
+++ prototypes/day06.py
@@ -44,15 +44,84 @@ I)SAN"""
     o.root, can_reach.leaf, can_reach.degree + 1 degree
     from orbits o
     join can_reach on o.leaf = can_reach.root
-    )
+    ), common_ancestors as (
+        select
+        root
+        from can_reach
+        where leaf = 'YOU'
+        or leaf = 'SAN'
+        group by root
+        having count(1) > 1
+    ), greatest_ancestor as (
     select
     *
-    from can_reach
-    where leaf = 'YOU'
-    or leaf = 'SAN'
-    order by root
+    from
+    can_reach
+    where
+    leaf in (select root from common_ancestors)
+    and root = 'COM'
+    order by degree desc
+    limit 1
+    ), distance as (
+    select
+    *
+    from
+    can_reach
+    where (leaf = 'SAN' or leaf = 'YOU')
+    and
+    root in (select leaf from greatest_ancestor)
+    )
+    select
+    sum(degree)
+    from
+    distance
     """).fetchall()
 
+def sql_transfer_distance(file_body):
+    cursor, conn = sql_orbital(file_body)
+    (c,) = cursor.execute("""
+    with recursive can_reach as (
+    select
+    root, leaf, 0 degree
+    from orbits
+    union all
+    select
+    o.root, can_reach.leaf, can_reach.degree + 1 degree
+    from orbits o
+    join can_reach on o.leaf = can_reach.root
+    ), common_ancestors as (
+        select
+        root
+        from can_reach
+        where leaf = 'YOU'
+        or leaf = 'SAN'
+        group by root
+        having count(1) > 1
+    ), greatest_ancestor as (
+    select
+    *
+    from
+    can_reach
+    where
+    leaf in (select root from common_ancestors)
+    and root = 'COM'
+    order by degree desc
+    limit 1
+    ), distance as (
+    select
+    *
+    from
+    can_reach
+    where (leaf = 'SAN' or leaf = 'YOU')
+    and
+    root in (select leaf from greatest_ancestor)
+    )
+    select
+    sum(degree)
+    from
+    distance
+    """).fetchone()
+    return c
 
 def sql_orbital_count(file_body):
     cursor, conn = sql_orbital(file_body)
@@ -77,3 +146,4 @@ def sql_orbital_count(file_body):
 with open("input/day06.txt") as f:
     file_body = f.read()
     print(sql_orbital_count(file_body))
+    print(sql_transfer_distance(file_body))
