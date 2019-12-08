a5997e23a9c0cd9319271bef5f116c4292b89601
Conrad Dean <conrad.p.dean@gmail.com>
2019-12-08 10:03:53 -0500

    some cleanup and implementing part2

--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,7 @@ pub fn main() -> std::io::Result<()> {
     let f = fs::read_to_string("input/day08.txt")?;
     let input_state = f.trim();
     dbg!(input_state.len());
-    let mut layers = vec![];
-    let step = (25 * 6);
-    let count_of_layers = input_state.len() / step;
-    for i in 0..(count_of_layers) {
-        let layer = &input_state[(i * step)..((i + 1) * step)];
-        layers.push(layer);
-    }
-    // check we have all the pixels
-    let mut pixel_count = 0;
-    for layer in &layers {
-        pixel_count += layer.chars().count();
-    }
-    dbg!(pixel_count);
+    let layers = image_to_layers(input_state, 25, 6);
     let target_layer = layers
         .iter()
         .min_by(|a, b| {
@@ -32,7 +20,54 @@ pub fn main() -> std::io::Result<()> {
     let twos = target_layer.chars().filter(|c| *c == '2').count();
     let part1 = ones * twos;
     dbg!(part1);
+    let data = "0222112222120000";
+    let layers_ex = image_to_layers(data, 2, 2);
+    dbg!(layers_to_image(layers_ex.clone()));
+    render(&layers_to_image(layers_ex), 2, 2);
+    render(&layers_to_image(layers), 25, 6);
     Ok(())
+}
+
+pub fn render(image: &str, x: usize, y: usize) {
+    let contrast: String = image
+        .chars()
+        // increase contrast so i can see the message
+        .map(|e| if e == '0' { '.' } else { '#' })
+        .collect();
+    for row in 0..y {
+        println!("{}", &contrast[x * row..x * (row + 1)]);
+    }
+}
+
+pub fn layers_to_image(layers: Vec<&str>) -> String {
+    layers
+        .iter()
+        .map(|e| e.chars())
+        .fold(layers[0].to_string(), |acc, e| {
+            let v: String = acc
+                .chars()
+                .zip(e)
+                .map(|(a, b)| match (a, b) {
+                    ('0', _) => '0',
+                    ('1', _) => '1',
+                    ('2', x) => x,
+                    (x, _) => panic!("got pixel val {}", x),
+                })
+                .collect();
+            v
+        })
+}
+
+pub fn image_to_layers(data: &str, x_width: usize, y_width: usize) -> Vec<&str> {
+    dbg!(data.len());
+    let mut layers: Vec<&str> = vec![];
+    let step = x_width * y_width;
+    let count_of_layers = data.len() / step;
+    for i in 0..(count_of_layers) {
+        let layer = &data[(i * step)..((i + 1) * step)];
+        layers.push(layer);
+    }
+    layers
 }
 
 #[cfg(test)]
@@ -40,5 +75,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {}
+    fn it_works() {
+        let data = "0222112222120000";
+        let layers = image_to_layers(data, 2, 2);
+    }
 }
