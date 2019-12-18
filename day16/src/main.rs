b9e44f6aa21d437de6929a53d8c0687bd690bf71
Conrad Dean <conrad.p.dean@gmail.com>
2019-12-18 10:12:13 -0500

    got python tests ported over to rust

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -27,9 +27,53 @@ pub fn base_pattern(ith_element: i32) -> Vec<i32> {
     expanded.cycle().skip(1).take(1000).cloned().collect()
 }
 
+struct FFT {
+    current: String,
+}
+
+impl FFT {
+    fn new(s: String) -> FFT {
+        FFT { current: s }
+    }
+}
+
+impl Iterator for FFT {
+    type Item = String;
+
+    fn next(&mut self) -> Option<String> {
+        let parsed: Vec<i32> = self
+            .current
+            .chars()
+            .map(|e| e.to_string().parse().unwrap())
+            .collect();
+        let mut output = vec![];
+        for (i, e) in parsed.iter().enumerate() {
+            let this_digit = parsed
+                .iter()
+                .zip(base_pattern(i as i32 + 1))
+                .map(|(left, right)| left * right)
+                .fold(0, |a, b| a + b);
+            output.push(this_digit.abs() % 10);
+        }
+        self.current = output
+            .iter()
+            .map(|d| d.to_string())
+            .collect::<Vec<String>>()
+            .join("");
+        Some(self.current.clone())
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_tiny() {
+        let mut fft = FFT::new("12345678".to_string());
+        assert_eq!(fft.next().unwrap(), "48226158");
+        assert_eq!(fft.next().unwrap(), "34040438");
+    }
 
     #[test]
     fn base1() {
